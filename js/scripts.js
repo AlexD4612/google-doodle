829ecc945296e7b6112aec7ee89e11a0e6331207
AlexD4612 <59583079+AlexD4612@users.noreply.github.com>
2020-02-19 02:28:59 -0800

    added animation and position

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-console.log('OK');
+gsap.to("svg#stand",{x:260,y:-15,duration:.0001,zIndex:20});
+gsap.to("svg#lens",{x:-245,y:-70,duration:.0001})
+gsap.to("svg#lensCap",{x:35,y:-70,duration:.0001})
+gsap.to("svg#lowerG",{x:36,y:-55})
+gsap.to("svg#e",{x:65,y:-75})
+gsap.to("svg#G",{x:110,y:-70})
+gsap.to("svg#back",{x:-10,y:-300})
+
+var animate = gsap.to("svg#lens",{delay:.5,rotate:90,yoyo:true,repeat:-1,duration:1,ease:Power1,repeatDelay:1})
+
+
+document.getElementById("button").addEventListener("click",flash); 
+
+
+function flash(){
+  gsap.timeline()
+    .to("#three",{zIndex:200,opacity:1})
+    .to("#three",{zIndex:-200,opacity:0,duration:1})
+    .to("#two",{zIndex:200,opacity:1})
+    .to("#two",{zIndex:-200,opacity:0,duration:1})
+    .to("#one",{zIndex:200,opacity:1})
+    .to("#one",{zIndex:-200,opacity:0,duration:1})
+    .to("#flash",{opacity:1,zIndex:999999})
+    .to("#flash",{opacity:0,zIndex:-99999,})
+       
+}
+gsap.to("svg#three",{x:50,duration:.00000000001})
+gsap.to("svg#two",{x:50,duration:.00000000001})
+gsap.to("svg#one",{x:50,duration:.00000000001})
+
+
+gsap.to("svg#button",{x:487,y:395,duration:.00000000001})
+
