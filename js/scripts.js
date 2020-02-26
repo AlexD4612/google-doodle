b59f5bcec6dc6284556366bbf5f51ce15daad476
AlexD4612 <59583079+AlexD4612@users.noreply.github.com>
2020-02-26 02:02:11 -0800

    added comments, fixed positioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,41 @@
-gsap.to("svg#stand",{x:310,y:40,duration:.0001,zIndex:20});
-gsap.to("svg#lens",{x:-245,y:-70,duration:.0001})
-gsap.to("svg#lensCap",{x:35,y:-70,duration:.0001})
-gsap.to("svg#lowerG",{x:36,y:-55})
-gsap.to("svg#e",{x:65,y:-75})
-gsap.to("svg#G",{x:120,y:-70})
+/*-----------------------------------------------
+function: positioning
+purpose: positions all objects, sets initial opacity, z-index
+------------------------------------------------*/
+
+gsap.to("svg#stand",{x:260,y:55,duration:.0001,zIndex:20});
+gsap.to("svg#lens",{x:-324,y:-70,duration:.0001})
+gsap.to("svg#lensCap",{x:-17,y:-70,duration:.0001})
+gsap.to("svg#lowerG",{x:-20,y:-42})
+gsap.to("svg#e",{x:0,y:-60})
+gsap.to("svg#G",{x:80,y:-70})
 gsap.to("svg#back",{x:-10,y:-300})
+gsap.to("svg#ShadowG",{x:110,y:-70})
+gsap.to("svg#three",{x:50,duration:.00000000001})
+gsap.to("svg#two",{x:50,duration:.00000000001})
+gsap.to("svg#one",{x:50,duration:.00000000001})
+gsap.to("svg#button",{x:487,y:395,duration:.00000000001})
+gsap.to("svg#prompt",{x:490,y:400,duration:.00000000001,opacity:0})  
+gsap.to("svg#prompt",{opacity:1,delay:5})  //end positioning
 
-var animate = gsap.to("svg#lens",{delay:.5,rotate:90,yoyo:true,repeat:-1,duration:1,ease:Power1,repeatDelay:1})
+/*---------------------------------------
+function: rotate
+purpose: rotate lens svg
+---------------------------------------*/
+var animate = gsap.to("svg#lens",{delay:.5,rotate:90,yoyo:true,repeat:-1,duration:1,ease:Power1,repeatDelay:1}) 
+// end rotate
 
 
+/*-----------------------------------------------------
+function:flash
+purpose: simulate camera flash when button is pressed
+-------------------------------------------------------*/
 document.getElementById("button").addEventListener("click",flash); 
 
 
 function flash(){
   gsap.timeline()
+    .to("svg#prompt",{opacity:0})
     .to("#three",{zIndex:200,opacity:1})
     .to("#three",{zIndex:-200,opacity:0,duration:1})
     .to("#two",{zIndex:200,opacity:1})
@@ -23,7 +45,14 @@ function flash(){
     .to("#flash",{opacity:1,zIndex:999999})
     .to("#flash",{opacity:0,zIndex:-99999,})
 
-}
+} // end flash
+
+/*-------------------------------------------------------------------
+function: sound
+purpose: add sound to camera shutter when button clicked
+****still not finished, commented out for testing****
+-------------------------------------------------------------------*/
+/*
 document.getElementById("button").addEventListener("click",setTimeout)
 setTimeout(function(){
     player.play();
@@ -31,14 +60,15 @@ setTimeout(function(){
 
 document.getElementById("player").addEventListener("ended",function(){
     player.currentTime=1;
-    player.pause();
-});
+    player.pause(); 
+}); 
+*/
 
 
-gsap.to("svg#three",{x:50,duration:.00000000001})
-gsap.to("svg#two",{x:50,duration:.00000000001})
-gsap.to("svg#one",{x:50,duration:.00000000001})
 
 
-gsap.to("svg#button",{x:487,y:395,duration:.00000000001})
+/*--------NOTES------------
+- In v2, animate letters to move during countdown and freeze before camera flash
 
+
+---------------------------*/
